f4511801e73f9c8c6f9e18ab60f3b836af571a38
Micael Lima <micaelcomp@alu.ufc.br>
2022-05-01 18:55:49 -0300

    fix: phpcs

--- src/Dtos/UpdateTalkDTO.php
+++ src/Dtos/UpdateTalkDTO.php
@@ -6,10 +6,10 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class UpdateTalkDTO extends DataTransferObject
 {
-  public string | null $title;
-  public string | null $description;
-  public string | null $date;
-  public int | null $speaker_id;
-  public string | null $start_time;
-  public string | null $end_time;
+    public string | null $title;
+    public string | null $description;
+    public string | null $date;
+    public int | null $speaker_id;
+    public string | null $start_time;
+    public string | null $end_time;
 }
