bd9c1c4cd9c9174672ebf71a3f33d3a8749e34ac
Micael Lima <micaelcomp@alu.ufc.br>
2022-05-03 13:33:22 -0300

    docs: show speaker route

--- src/Controller/ShowSpeakerController.php
+++ src/Controller/ShowSpeakerController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Services\ShowSpeakerService;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use App\Entity\Speaker;
 
 class ShowSpeakerController
 {
@@ -17,9 +20,25 @@ class ShowSpeakerController
         $this->showSpeakerService = $showSpeakerService;
     }
 
-  /**
-   * @Route("/speakers/{id}", methods={"GET"})
-   */
+    /**
+    * Show speaker route.
+    * @Route("/speakers/{id}", methods={"GET"})
+    * @OA\Parameter(
+    *    name="id",
+    *    in="path",
+    *    description="The field used to identify speaker",
+    * )
+    *
+    * @OA\Response(
+    *     response=200,
+    *     description="Returns a Speaker on success",
+    *     @Model(type=Speaker::class)
+    * )
+    * @OA\Response(
+    *     response=404,
+    *     description="Return 404 if a inexistent speaker is provided",
+    * )
+    */
     public function handle(int $id, Request $request): Response
     {
         try {
@@ -29,7 +48,6 @@ class ShowSpeakerController
             ->setStatusCode(200)
             ->setData($response->toJson());
         } catch (\Exception $error) {
-            var_dump($error->__toString());
             return (new JsonResponse())
             ->setStatusCode($error->getCode())
             ->setData($error->__toString());
