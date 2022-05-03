7bdcfe9e92cede4bf270d4e119b1518b0d250056
Micael Lima <micaelcomp@alu.ufc.br>
2022-05-03 12:13:01 -0300

    feat: use global DateTime

--- src/Dtos/CreateEventDTO.php
+++ src/Dtos/CreateEventDTO.php
@@ -7,8 +7,8 @@ use Spatie\DataTransferObject\DataTransferObject;
 class CreateEventDTO extends DataTransferObject
 {
     public string $title;
-    public DateTime | string $start_date;
-    public DateTime | string $end_date;
+    public \DateTime | string $start_date;
+    public \DateTime | string $end_date;
     public string $description;
     public string $status;
 }
