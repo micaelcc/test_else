c8393a36abb61730e2b22686f182361ac154a411
Micael Lima <micaelcomp@alu.ufc.br>
2022-05-03 12:27:03 -0300

    docs: create talk route

--- src/Controller/CreateTalkController.php
+++ src/Controller/CreateTalkController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Dtos\CreateTalkDTO;
 use App\Services\CreateTalkService;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use App\Entity\Talk;
 
 class CreateTalkController
 {
@@ -19,7 +22,25 @@ class CreateTalkController
     }
 
   /**
+   * Create talk route.
    * @Route("/talks", methods={"POST"})
+   * @OA\RequestBody(
+   *    @Model(type=CreateTalkDTO::class)
+   * )
+   *
+   * @OA\Response(
+   *     response=200,
+   *     description="Returns an talk on success",
+   *     @Model(type=Talk::class)
+   * )
+   * @OA\Response(
+   *     response=400,
+   *     description="Return 400 if a invalid data is provided",
+   * )
+   * @OA\Response(
+   *     response=404,
+   *     description="Return 404 if a not existent event or speaker is provided",
+   * )
    */
     public function handle(Request $request): Response
     {
